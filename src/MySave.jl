ff8faac0e2e3d86d96db1f07284c13f7da8cbac5
ryubee1980 <ryubee@gmail.com>
2021-09-27 11:36:29 +0900

    corrected

--- src/MySave.jl
+++ src/MySave.jl
@@ -34,7 +34,7 @@ const dir_savevar = Ref(".")
 is the filename string to which `@savevar` saves the value of a variable.
 """
 fn_savevar(x::Symbol) = joinpath(dir_savevar[], string(x) * ".txt")
-fn_savevar(n::String,x::Symbol)=joinpath(dir_savevar[], string(x)*"_"*n*".txt")
+fn_savevar(n::Int64,x::Symbol)=joinpath(dir_savevar[], string(x)*"_"*string(n)*".txt")
 
 """
     @savevar(args...)
