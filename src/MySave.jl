2905b3bc834ed22ed7bffa30c23f451623ce4c1d
ryubee1980 <ryubee@gmail.com>
2022-02-18 17:04:06 +0900

    test

--- src/MySave.jl
+++ src/MySave.jl
@@ -129,7 +129,7 @@ Example 1: `fnum[]=3;@savevarn x y` saves the variables x and y in the files `x_
 
 Example 2: `fnum[]=3;X, Y=@loadvarn x y` load the values of x and y from the files `x_3.txt` and `y_3.txt`.
 """
-const   fnum=Ref(0)
+#const   fnum=Ref(0)
 
 """
     @savevarn(args...)
@@ -139,7 +139,7 @@ saves the variables in args to the corresponding textfiles.
 Example: `@savevarn A B C` saves the variables `A`, `B`, `C` to textfiles. 
 The names of the files are `A_fnum[].txt`, `B_fnum[].txt`, `C_fnum[].txt`.
 """
-macro savevarn(args...)
+macro savevarn(fnum,args...)
     A = [:(savevar($(fn_savevar(fnum,x)), $(esc(x)))) for x in args]
     quote $(A...); nothing end
 end
