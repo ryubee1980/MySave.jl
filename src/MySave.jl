3771c5fbea34a86ee0190c2fb9fc636df4e8f257
ryubee1980 <ryubee@gmail.com>
2022-02-19 01:14:41 +0900

    test

--- src/MySave.jl
+++ src/MySave.jl
@@ -66,9 +66,9 @@ is the filename string to which `@savevar` saves the value of a variable.
 """
 fn_savevar(x::Symbol) = joinpath(dir_savevar[], string(x) * ".txt")
 #fn_savevar(n::Base.RefValue{Int64},x::Symbol)=joinpath(dir_savevar[], string(x)*"_"*string(n[])*".txt")
-fn_savevar(n::Symbol,x::Symbol)=joinpath(dir_savevar[], string(x)*"_"*string(n)*".txt")
+#fn_savevar(n::Symbol,x::Symbol)=joinpath(dir_savevar[], string(x)*"_"*string(n)*".txt")
 fn_savevar(n::Base.RefValue{Int64},x::Symbol)=joinpath(dir_savevar[], string(x)*"_"*string(n[])*".txt")
-fn_savevar(n::Int64,x::Symbol)=joinpath(dir_savevar[], string(x)*"_"*string(n)*".txt")
+#fn_savevar(n::Int64,x::Symbol)=joinpath(dir_savevar[], string(x)*"_"*string(n)*".txt")
 
 
 """
