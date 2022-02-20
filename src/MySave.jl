c7d7fd8c285da28a7a7dd6ce480b0e68bbb919df
ryubee1980 <ryubee@gmail.com>
2022-02-20 21:47:47 +0900

    test

--- src/MySave.jl
+++ src/MySave.jl
@@ -32,12 +32,14 @@ greet() = print("Hello My module for saving and loading files!")
 saves the value of `x` to the file `fn`, where `fn` is the filename string of the file.
 """
 savevar(fn, x) = write(fn, string(x))
-function savevar_num(n,x,xsym)  
-    fn=joinpath(dir_savevar[], string(xsym)*"_"*string(n)*".txt")
+function savevar_num(n,x,xstr::String)  
+    fn=joinpath(dir_savevar[], xstr*"_"*string(n)*".txt")
     savevar(fn, x)
     nothing
 end
-
+function xstrng(xsym::Symbol)
+    string(xsym)
+end
 
 """
     loadvar(fn)
@@ -140,7 +142,7 @@ The names of the files are `A_fnum.txt`, `B_fnum.txt`, `C_fnum.txt`.
 """
 macro savevarn(fnum,args...)
     #A = [quote savevar($(fn_savevar(Ref(fnum),x)), $(esc(x))) end for x in args]
-    A = [quote savevar_num($(esc(fnum)), $(esc(x)), esc(x)) end for x in args]
+    A = [quote savevar_num($(esc(fnum)), $(esc(x)), $(xstrng(x))) end for x in args]
     
    # A = [quote fnumV=$fnum;savevar($(fn_savevar(fnumV,x)), $(esc(x))) end for x in args]
     
